fe7ad27fccb7f3e86af0fe74266fde95eefa71d0
Kyle R <KyleRichVA@gmail.com>
2015-08-05 13:56:43 -0700

    8-5-15
    Added on scoring methods to the player class.

--- PythonDicePoker.py
+++ PythonDicePoker.py
@@ -5,8 +5,20 @@
 '''
 import random
 import string
+# AI constants
 HUMAN = True
 AI = False
+
+# 'hand' value constants
+NOTHING = 0
+PAIR = 1
+TWOPAIR = 2
+THREEOFKIND = 3
+FIVEHIGHSTRAIGHT = 4 # dice 1-5 inclusive
+SIXHIGHSTRAIGHT = 5 # dice 2-6 inclusive
+FULLHOUSE = 6 # Pair + 3of Kind
+FOUROFKIND = 7
+FIVEOFKIND = 8
 
 class Die(object):
 	# Represents a single die in the game.
@@ -46,12 +58,59 @@ class Player(object):
 		for die in self.dice:
 			die.roll()
 
+	def score(self):
+		#funtion that takes a list of die and returns a score based off the dice
+		numPairs = 0
+		trip = False
+
+		# Five of Kind Check
+		five = True
+		checkVal = self.dice[0].value
+		for die in self.dice[1::]:
+			if die.value != checkVal:
+				five = False
+				break
+		if five == True:
+			return FIVEOFKIND
+
+		# Four of Kind/Full House/Trips/Pair Check
+		count = 0
+		for die1 in self.dice:
+			for die2 in self.dice:
+				if die1.value == die2.value:
+					count += 1
+			if count == 4:
+				return FOUROFKIND
+			if count == 3:
+				trip = True
+			if count == 2:
+				numPairs += 1
+			count = 0
+		if trip and numPairs == 2:
+			return FULLHOUSE
+		if trip:
+			return THREEOFKIND
+		if numPairs == 4:
+			return TWOPAIR
+		if numPairs == 2:
+			return PAIR
+
+		# six or five high straight (2-6)/(1-5) check
+		diceVals = []
+		for die in self.dice:
+			diceVals.append(die.value)
+		diceVals.sort()
+		if diceVals == range(2,7):
+			return SIXHIGHSTRAIGHT
+		if diceVals == range(1,6):
+			return FIVEHIGHSTRAIGHT
+
+		return NOTHING
+
 #testing stuff
-print "But then changed it back in Mac OS"
 me = Player(HUMAN,"Kyle")
 me.makeRolls()
 me.report()
-me.makeRolls('1 5')
-me.report()
+print "Your score is:", me.score()
 
 
